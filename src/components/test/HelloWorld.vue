86a55527346e5d8c3d5fb9899299f87d13f02d04
jason <1986tianxie@sina.com>
2019-06-13 18:11:37 +0800

    init

---
+++ src/components/test/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test">
+    this is a test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.test {
+    color: red;
+}
+</style>
